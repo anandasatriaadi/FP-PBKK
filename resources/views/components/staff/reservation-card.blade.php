e0db4651fb7ce0118ce02d7b865fe8897217c9d9
anandasatriaadi <putu.satriaadi@gmail.com>
2022-05-28 23:47:19 +0700

    style: add state in reservation card

--- resources/views/components/staff/reservation-card.blade.php
+++ resources/views/components/staff/reservation-card.blade.php
@@ -1,3 +1,5 @@
+@props(['status' => false])
+
 <div class="w-full sm:w-1/2 lg:w-1/3 p-2">
     <div class="shadow-lg rounded-2xl p-4 bg-white w-full">
         <div class="mb-4">
@@ -16,6 +18,7 @@
                 </span>
             </div>
         </div>
+        @if ($status == true)
         <div class="flex flex-row gap-2">
             <div class="py-1 px-2 flex-1 bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg  cursor-pointer">
                 Complete
@@ -24,5 +27,12 @@
                 Cancel
             </div>
         </div>
+        @else
+        <div class="flex flex-row gap-2">
+            <div class="py-1 px-2 flex-1 bg-slate-600 text-white text-center text-base font-semibold shadow-md rounded-lg">
+                Completed
+            </div>
+        </div>
+        @endif
     </div>
 </div>
