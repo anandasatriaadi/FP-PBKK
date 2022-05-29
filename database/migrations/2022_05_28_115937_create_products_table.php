bb5eae8815485014265d19386bd085e1fa83f0c3
anandasatriaadi <putu.satriaadi@gmail.com>
2022-05-29 16:42:06 +0700

    fix: update product migration

--- database/migrations/2022_05_28_115937_create_products_table.php
+++ database/migrations/2022_05_28_115937_create_products_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->longText('name');
-            $table->decimal('price', 15,2);
             $table->longText('description');
-            $table->longText('badge');
+            $table->decimal('price', 15, 2);
+            $table->longText('image');
             $table->timestamps();
         });
     }
